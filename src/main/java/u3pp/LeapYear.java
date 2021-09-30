366e126df12ef73db5641f28c8456e4657f2727b
jach571 <jach571@vcs.email>
2021-09-30 13:44:26 -0700

    Unit 3 Project

--- src/main/java/u3pp/LeapYear.java
+++ src/main/java/u3pp/LeapYear.java
@@ -1,17 +1,28 @@
 package u3pp;
 
 /**
- * YOUR COMMENTs HERE
+ * The code determines what years are leap years by testing the following constraints.
+ * Every year between 1582 and now that is exactly divisible by four is a leap year.
+   - Years that are evenly divisble by 100 are not leap years unless they are evenly divisible by 400
+   - - Example: 1700, 1800, and 1900 are not leap years
+   - - Example: 1600 and 2000 are leap years
  * 
- * @author //YOUR NAME HERE
+ * @author Jacob Cho
  *
  */
 
 public class LeapYear {
-	/**
-	 * YOUR COMMENTS HERE
-	 */
 	public static boolean isLeapYear(int year) {
-		/* YOUR CODE HERE */
+	//Checks if the year is after 1582
+		if(year < 1582){
+			return false; 
+	//Checks if the year is exactly divisible by 4, evenly divisible by 100, or evenly divisible by 400
+	//If the year is evenly divisible by 100, it is not a leap year unless it is also divisible by 400
+		} else if(year >= 1582 && year % 4 == 0 && year % 100 != 0 || year % 400 == 0){
+			return true;
+	//If none of the conditions are met, then the function returns false since the year is a leap year
+		} else{
+			return false;
+		}
 	}
 }
--- src/main/java/u3pp/Main.java
+++ src/main/java/u3pp/Main.java
@@ -3,25 +3,35 @@ package u3pp;
 import java.util.Scanner;
 
 /**
- * YOUR COMMENTS HERE
+ * The main method makes a scanner, and prompts the user to enter a year. Once the user enters a year, a method determines whether
+ or not that year is a leap year. If the user types the exact string "STOP" then the program will end.
  * 
- * @author YOUR NAME HERE
+ * @author Jacob Cho
  */
 
 class Main {
 
 	public static void main(String[] args) {
 
-		//YOUR CODE HERE
+		Scanner scan = new Scanner(System.in); //Creates a scanner
+		String input = "";
+		int year = 0;
 
 		System.out.print("Enter a year (or enter STOP to end): ");
-		String input = sc.nextLine();
+		input = scan.nextLine();
 
 		while (!input.toUpperCase().equals("STOP")) {
-			/* YOUR CODE HERE */
+			year = Integer.parseInt(input);
+
+			if(LeapYear.isLeapYear(year) == true){
+				System.out.println(year + " is a leap year");
+			} else{
+				System.out.println(year + " is not a leap year");
+			}
+
 			System.out.print("Enter a year (or enter STOP to end): ");
-			//YOUR CODE HERE
+			input = scan.nextLine();
 		}
-		//YOUR CODE HERE
+		scan.close();
 	}
 }
